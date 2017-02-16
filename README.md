4f967ccf14e0afd82bc3f3ce48513ab92d6cba7a
Seve Sheldon <sevesheldon@gmail.com>
2017-02-16 14:13:35 -0800

    add finished and functional HTML, CSS and README files along with a JS file that includes functioning and separated business and user logic.

---
+++ README.md
@@ -0,0 +1,52 @@
+# movie-tickets
+
+##### Epicodus Section: JavaScript OO - Movie Tickets
+
+#### By Seve Sheldon
+
+## Description
+
+This application is a demonstration of constructors, instances and prototypes using JavaScript to create a simple app that can calculate the cost of movie tickets.
+
+## Setup
+
+Install movie-tickets by cloning this repository.
+
+## Specs
+
+* User fills out the selected fields and the ticket calculator will compute the price.
+
+## Technologies Used
+HTML
+
+CSS
+
+Bootstrap
+
+JavaScript
+
+jQuery
+
+### Legal
+
+Copyright (c) 2017 **Epicodus**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 //business logic
 
-function Ticket(movie, time, age, quantity) {
-  this.movie = movie;
-  this.time = time;
+function Ticket(age, time, movie, quantity) {
   this.age = age;
+  this.time = time;
+  this.movie = movie;
   this.quantity = quantity;
 }
 
-Ticket.protoype.price = function() {
+Ticket.prototype.price = function() {
   var ticketPrice = 12;
   if ((this.age <= 8) || (this.age >= 65)) {
     ticketPrice *= .9;
@@ -15,16 +15,14 @@ Ticket.protoype.price = function() {
   if (this.time < 17) {
     ticketPrice -= 2;
   }
-  if ((this.movie === "Lego Batman") || (this.movie === "Lala Land")) {  //use id or actual name??
+  if ((this.movie === "Lego Batman") || (this.movie === "Lala Land")) {
     ticketPrice += 2;
   }
   if (this.quantity > 1) {
-    ticketPrice * this.quantity;
+    ticketPrice *= this.quantity;
   }
   return ticketPrice;
 }
-
-
 
 //user-interface logic
 $(document).ready(function() {
@@ -33,15 +31,19 @@ $(document).ready(function() {
 
     var inputtedAge = parseInt($("input#age").val());
     var selectedTime = parseFloat($("select#time").val());
+    var timeStandard = $("select#time option:selected").text();
     var selectedMovie = $("select#movie-title").val();
     var inputtedQuantity = parseInt($("input#quantity").val());
-
-    console.log(inputtedAge);
-    console.log(selectedTime);
-    console.log(selectedMovie);
-    console.log(inputtedQuantity);
+    var newTicket = new Ticket(inputtedAge, selectedTime, selectedMovie, inputtedQuantity);
+    var ticketPrice = newTicket.price();
 
     $("#ticket-price-display").show();
+    $("#ticket-price-display h3").text("$ " + ticketPrice);
+    $("ul#ticket-info").append('<li>' + "Age: " + inputtedAge + '</li>');
+    $("ul#ticket-info").append('<li>' + "Time: " + timeStandard + '</li>');
+    $("ul#ticket-info").append('<li>' + "Movie: " + selectedMovie + '</li>');
+    $("ul#ticket-info").append('<li>' + "Number of tickets: " + inputtedQuantity + '</li>');
+
   });
 
 });
